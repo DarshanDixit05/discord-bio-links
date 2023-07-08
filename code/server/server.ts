a3170729c87f58eee922d92b103e801faf6dc7c0
main <zzyg675@proton.me>
2023-07-08 18:45:43 +0400

    rate limit, ejs view engine, wrapped routers in try...catch

--- code/server/server.ts
+++ code/server/server.ts
@@ -1,16 +1,30 @@
 import express from "express";
 import { allFilesInDir } from "../helpers/allFilesInDir.js";
 import { cfg } from "../config.js";
-import { relative } from "path";
+import { relative, resolve } from "path";
 
 const appServer = express();
+appServer.set("trust proxy", cfg.numberOfProxies);
+// See ip.ts for more info.
+
 const routePaths: string[] = await allFilesInDir(cfg.directories.routes, true);
 
 for (const routePath of routePaths) {
     const route = (await import(`file://${routePath}`)).default;
-    appServer.use("", route);
+
+    appServer.use("", async function (req, res, next) {
+        try {
+            await route(req, res, next);
+        } catch (err) {
+            console.error(err);
+        }
+    });
+
     console.log(`Loaded route ${relative(cfg.directories.routes, routePath)} !`);
 }
+
+appServer.set('views', resolve(cfg.directories.public, "views"));
+appServer.set('view engine', 'ejs');
 
 export { appServer };
 
