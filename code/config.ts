46456e62731b75302e046bd74145fc81c316abd3
main <zzyg675@proton.me>
2023-07-08 18:46:27 +0400

    checks for valid server port, rate limit

--- code/config.ts
+++ code/config.ts
@@ -9,11 +9,14 @@ const envFilePath = resolve(__dirname, "..", `.${envName}.env`);
 
 dotenv.config({ path: envFilePath });
 
+if (process.env.SERVER_PORT === undefined || process.env.SERVER_PORT === "" || isNaN(parseInt(`${process.env.SERVER_PORT}`))) throw new Error(`Missing/Invalid SERVER_PORT in ${envFilePath}`);
+if (process.env.NUMBER_OF_PROXIES === undefined || process.env.NUMBER_OF_PROXIES === "" || isNaN(parseInt(`${process.env.NUMBER_OF_PROXIES}`))) throw new Error(`Missing/Invalid NUMBER_OF_PROXIES in ${envFilePath}`);
 if (process.env.CLIENT_ID === undefined || process.env.CLIENT_ID === "") throw new Error(`Missing CLIENT_ID in ${envFilePath}`);
 if (process.env.CLIENT_SECRET === undefined || process.env.CLIENT_SECRET === "") throw new Error(`Missing CLIENT_SECRET in ${envFilePath}`);
 
 
 export const cfg = {
+    numberOfProxies: parseInt(`${process.env.NUMBER_OF_PROXIES}`),
     port: parseInt(`${process.env.SERVER_PORT}`),
     client: {
         id: process.env.CLIENT_ID,
