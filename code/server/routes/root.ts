1ddcf4bb6f73e2b7c1af59f82c00414214344c73
main <zzyg675@proton.me>
2023-07-08 18:45:15 +0400

    support for multiple languages, switched from html to ejs views

--- code/server/routes/root.ts
+++ code/server/routes/root.ts
@@ -1,12 +1,26 @@
 import express, { Router } from "express";
+import { resolve } from "path";
 import { cfg } from "../../config.js";
+import { join } from "path";
+import rateLimit from 'express-rate-limit'
 
 const router = Router();
 
-router.use("/", express.static(cfg.directories.public));
+router.use("/", express.static(resolve(cfg.directories.public)));
 
 router.get("/", function (req, res) {
-    res.sendFile("/pages/index.html", { root: cfg.directories.public })
+    const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+
+    for (const lang of acceptedLanguages) {
+        if (!res.headersSent) {
+            if (lang.includes("fr")) {
+                res.render(join("french", "index.ejs"));
+            }
+        }
+    }
+
+    // Fallback language: English
+    if (!res.headersSent) res.render(join("english", "index.ejs"));
 });
 
 export default router;
