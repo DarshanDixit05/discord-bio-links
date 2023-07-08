7ea5d7d2fb516e3388c90ecb8459d539e4d1e6bd
main <zzyg675@proton.me>
2023-07-08 18:44:29 +0400

    rate limit

--- code/server/routes/api/root.ts
+++ code/server/routes/api/root.ts
@@ -1,7 +1,16 @@
 import express, { Router } from "express";
+import rateLimit from "express-rate-limit";
 
 const router = Router();
 
+const limiter = rateLimit({
+    windowMs: 1000 * 60 * 3,
+    max: 15,
+    standardHeaders: true,
+    message: { message: "You are making too many requests. Try again later.", code: "RATE_LIMITED" }
+});
+
+router.use("/api", limiter);
 router.use("/api", express.json());
 
 export default router;
