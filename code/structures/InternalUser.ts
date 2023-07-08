39345a1816275899b4730cb45b41a1141725fd9d
main <zzyg675@proton.me>
2023-07-08 18:46:00 +0400

    better error handling, static has() method, support for multiples biographies in one user, delete() method, revokeToken() method, save() is now public

--- code/structures/InternalUser.ts
+++ code/structures/InternalUser.ts
@@ -8,20 +8,31 @@ import { PartialDiscordUserObject } from "./discord/PartialDiscordUserObject.js"
 import { DiscordAccessTokenReponse } from "./discord/AccessTokenResponse.js";
 import { Session } from "./Session.js";
 import { randomBytes } from "crypto";
-import { CodifiedError } from "./CodifiedError.js";
+import DOMPurify from 'isomorphic-dompurify';
+import { isValidDiscordId } from "../helpers/isValidDiscordId.js";
+import { lang } from "../languages.js";
+import { revokeToken } from "../helpers/revokeToken.js";
 
 const keyv = new Keyv(`sqlite://${cfg.databasePaths.users}`);
 
 export class InternalUser {
     static readonly TEN_DAYS_IN_MILLISECONDS: number = 1 * 1000 * 60 * 60 * 24 * 10;
     public readonly id: string;
-    private _biography: Biography;
+    private _biographies: Record<keyof typeof lang, Biography>;
     private _refreshToken: string;
     private _session: Session;
 
     constructor(id: string) {
+        if (!isValidDiscordId(id)) throw new TypeError("NOT_A_DISCORD_ID");
         this.id = id;
-        this._biography = { text: "" };
+        this._biographies = {
+            us: {
+                text: ""
+            },
+            fr: {
+                text: ""
+            }
+        };
         this._refreshToken = "";
         this._session = {
             id: this.id,
@@ -46,32 +57,55 @@ export class InternalUser {
     static toUser(internalUser: InternalUser, partialDiscordUser: PartialDiscordUserObject): User {
         return {
             id: internalUser.id,
-            biography: internalUser._biography,
+            biographies: internalUser._biographies,
             avatarHash: partialDiscordUser.avatar || "",
-            username: partialDiscordUser.global_name || partialDiscordUser.username
+            username: partialDiscordUser.username,
+            displayName: partialDiscordUser.global_name || partialDiscordUser.username
         };
     }
 
+    static async has(id: string): Promise<boolean> {
+        return (await keyv.has(id));
+    }
+
     private async _fetch(): Promise<InternalUser> {
-        const rawFetched = await keyv.get(this.id) || {};
+        const rawFetched = await keyv.get(this.id);
+        if (rawFetched === undefined) throw new Error("UNKNOWN_INTERNAL_USER");
         const fetched: InternalUser = Object.assign(new InternalUser(this.id), rawFetched);
         return fetched;
     }
 
-    private async _save(): Promise<true> {
+    public async delete(): Promise<void> {
+        await keyv.delete(this.id);
+    }
+
+    public async save(): Promise<true> {
         return await keyv.set(this.id, this);
     }
 
     public async updateRefreshToken(newRefreshToken: string): Promise<true> {
         const fetchedThis: InternalUser = await this._fetch();
         fetchedThis._refreshToken = newRefreshToken;
-        return await fetchedThis._save();
+        return await fetchedThis.save();
+    }
+
+    public async revokeToken(): Promise<true> {
+        const tokenData = await this._getNewAccessToken();
+        await revokeToken(tokenData.access_token);
+        return true;
     }
 
     private async _updateSession(newSession: Session): Promise<true> {
         const fetchedThis: InternalUser = await this._fetch();
         fetchedThis._session = newSession
-        return await fetchedThis._save();
+        return await fetchedThis.save();
+    }
+
+    private async _getNewAccessToken(): Promise<DiscordAccessTokenReponse> {
+        const fetchedThis: InternalUser = await this._fetch();
+        const freshTokenData = await getFreshTokenData(fetchedThis._refreshToken);
+        await fetchedThis.updateRefreshToken(freshTokenData.refresh_token);
+        return freshTokenData;
     }
 
     public async getNewSession(): Promise<Session> {
@@ -85,32 +119,24 @@ export class InternalUser {
         return newSession;
     }
 
-    private async _getNewAccessToken(): Promise<DiscordAccessTokenReponse> {
+    public async editBios(newBios: Record<keyof typeof lang, Biography>): Promise<true> {
         const fetchedThis: InternalUser = await this._fetch();
-        console.log(this, fetchedThis);
-        if (fetchedThis._refreshToken === "") throw new TypeError(`Refresh token is empty string!`);
-        const freshTokenData = await getFreshTokenData(fetchedThis._refreshToken);
-        await fetchedThis.updateRefreshToken(freshTokenData.refresh_token);
-        return freshTokenData;
-    }
 
-    public async editBio(newBio: Biography): Promise<true> {
-        const fetchedThis: InternalUser = await this._fetch();
-        fetchedThis._biography = newBio;
-        return await fetchedThis._save();
+        for (const bio of Object.values(newBios)) {
+            bio.text = DOMPurify.sanitize(bio.text, { USE_PROFILES: { html: true } })
+        }
+
+        fetchedThis._biographies = newBios;
+        return await fetchedThis.save();
     }
 
     public async get(): Promise<User> {
-        try {
-            const fetchedThis: InternalUser = await this._fetch();
-            const freshTokenData = await fetchedThis._getNewAccessToken();
+        const fetchedThis: InternalUser = await this._fetch();
+        const freshTokenData = await fetchedThis._getNewAccessToken();
 
-            const partialDiscordUserData = await getPartialDiscordUserData(freshTokenData.access_token, freshTokenData.token_type);
-            return InternalUser.toUser(fetchedThis, partialDiscordUserData);
-        } catch (err) {
-            console.error(err);
-            throw new CodifiedError("UNKNOWN_INTERNAL_USER");
-        }
+        const partialDiscordUserData = await getPartialDiscordUserData(freshTokenData.access_token, freshTokenData.token_type);
+        return InternalUser.toUser(fetchedThis, partialDiscordUserData);
+
     }
 
     public async sessionTokenIs(token: string): Promise<boolean> {
