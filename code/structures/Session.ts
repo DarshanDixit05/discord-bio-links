85ffaffc5cec816d7c51840b384d550af2624084
main <zzyg675@proton.me>
2023-07-08 18:46:09 +0400

    isSession helper

--- code/structures/Session.ts
+++ code/structures/Session.ts
@@ -3,3 +3,16 @@ export interface Session {
     expirationTimestamp: number;
     id: string;
 }
+
+export function isSession(o: any): o is Session {
+    if (o) {
+        if (typeof o === "object") {
+            if ("token" in o && "expirationTimestamp" in o && "id" in o) {
+                if (typeof o.token === "string" && typeof o.expirationTimestamp === "number" && typeof o.id === "string") {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
