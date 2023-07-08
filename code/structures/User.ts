d339fc621f9f42d8611307781e9394e9318277ae
main <zzyg675@proton.me>
2023-07-08 18:46:17 +0400

    add distinction between Discord username & display names, support for multiple biographies in one user

--- code/structures/User.ts
+++ code/structures/User.ts
@@ -1,8 +1,10 @@
+import { lang } from "../languages.js";
 import { Biography } from "./Biography.js";
 
 export interface User {
     id: string;
     username: string;
+    displayName: string;
     avatarHash: string;
-    biography: Biography;
+    biographies: Record<keyof typeof lang, Biography>;
 }
