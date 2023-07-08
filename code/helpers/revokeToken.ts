ade2214234db9672b486699c79c2813994990e0a
main <zzyg675@proton.me>
2023-07-08 18:44:10 +0400

    revokeToken helper

---
+++ code/helpers/revokeToken.ts
@@ -0,0 +1,22 @@
+import { cfg } from "../config.js";
+import { PartialDiscordUserObject, isPartialDiscordUserObject } from "../structures/discord/PartialDiscordUserObject.js";
+import { doRequest } from "./doRequest.js";
+
+export async function revokeToken(accessToken: string): Promise<unknown> {
+    const url = "https://discord.com/api/oauth2/token/revoke";
+
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    };
+
+    const body: string = new URLSearchParams({
+        "client_id": cfg.client.id,
+        "client_secret": cfg.client.secret,
+        "token": accessToken
+    }).toString();
+
+    const response = await doRequest("POST", url, headers, body);
+    console.log(response);
+
+    return response;
+}
