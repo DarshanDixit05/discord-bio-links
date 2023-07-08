c06bc150c0ce8088c4045828d22ba2b7bd3f0794
main <zzyg675@proton.me>
2023-07-08 19:00:18 +0400

    better logging

--- public/js/helpers/doRequest.js
+++ public/js/helpers/doRequest.js
@@ -11,15 +11,32 @@
  * @param {RequestOptions} param0 
  */
 export function doRequest({ url, method, headers, body }) {
+    console.group(`${method} request at ${url}`);
+    console.log(`Headers:`);
+    console.dir(headers);
+    console.log(`Body:`);
+    console.dir(body);
+
     if (headers) headers["Content-Type"] = "application/json";
+    if (body) body = JSON.stringify(body);
+
+    const req = {
+        method, headers, body
+    };
 
     return new Promise((resolve, reject) => {
-        fetch(url, {
-            method,
-            headers,
-            body: JSON.stringify(body)
-        })
-            .then(response => resolve(response))
-            .catch(err => reject(err));
+        fetch(url, req)
+            .then(response => {
+                console.log(`Response:`);
+                console.dir(response);
+                console.groupEnd();
+                resolve(response);
+            })
+            .catch(err => {
+                console.log(`Error:`);
+                console.dir(err);
+                console.groupEnd();
+                reject(err);
+            });
     });
 }
