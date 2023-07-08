d4a2f699133d59140b91c16d16113a3b8738a7b3
main <zzyg675@proton.me>
2023-07-08 18:57:12 +0400

    remove files

--- code/structures/CodifiedError.ts
+++
@@ -1,9 +0,0 @@
-export class CodifiedError extends Error {
-    public readonly code: string;
-
-    constructor(code: string, message?: string) {
-        super(message);
-        this.name = "CodifiedError";
-        this.code = code;
-    }
-};
