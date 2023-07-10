1973917f557753218c5b0fb5bd891efca183ab36
main <zzyg675@proton.me>
2023-07-10 21:08:08 +0400

    not needed

--- code/structures/ErrorRequestReplyOptions.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from "express";
-
-export interface ErrorRequestReplyOptions {
-    res: Response;
-    httpCode: number;
-    errorCode: string;
-    errorMessage: string;
-}
--- public/js/types.js
+++
@@ -1,6 +0,0 @@
-/**
- * @typedef {object} Session
- * @property {number} expirationTimestamp
- * @property {string} token
- * @property {string} id
- */
