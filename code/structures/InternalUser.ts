f150bb9331196b384ec56a3060ec1cd4cb9232ee
main <zzyg675@proton.me>
2023-07-10 21:08:19 +0400

    turns sessions into strings

--- code/structures/InternalUser.ts
+++ code/structures/InternalUser.ts
@@ -6,21 +6,21 @@ import { getFreshTokenData } from "../helpers/getFreshTokenData.js";
 import { getPartialDiscordUserData } from "../helpers/getPartialDiscordUserData.js";
 import { PartialDiscordUserObject } from "./discord/PartialDiscordUserObject.js";
 import { DiscordAccessTokenReponse } from "./discord/AccessTokenResponse.js";
-import { Session } from "./Session.js";
+import { SessionToken } from "./Session.js";
 import { randomBytes } from "crypto";
 import DOMPurify from 'isomorphic-dompurify';
 import { isValidDiscordId } from "../helpers/isValidDiscordId.js";
 import { lang } from "../languages.js";
 import { revokeToken } from "../helpers/revokeToken.js";
 
-const keyv = new Keyv(`sqlite://${cfg.databasePaths.users}`);
+const users = new Keyv(`sqlite://${cfg.databasePaths.users}`);
+const sessions = new Keyv(`sqlite://${cfg.databasePaths.sessions}`);
 
 export class InternalUser {
     static readonly TEN_DAYS_IN_MILLISECONDS: number = 1 * 1000 * 60 * 60 * 24 * 10;
     public readonly id: string;
     private _biographies: Record<keyof typeof lang, Biography>;
     private _refreshToken: string;
-    private _session: Session;
 
     constructor(id: string) {
         if (!isValidDiscordId(id)) throw new TypeError("NOT_A_DISCORD_ID");
@@ -34,11 +34,6 @@ export class InternalUser {
             }
         };
         this._refreshToken = "";
-        this._session = {
-            id: this.id,
-            token: "",
-            expirationTimestamp: 0
-        };
     }
 
     static isInternalUser(o: unknown): o is InternalUser {
@@ -65,22 +60,22 @@ export class InternalUser {
     }
 
     static async has(id: string): Promise<boolean> {
-        return (await keyv.has(id));
+        return (await users.has(id));
     }
 
     private async _fetch(): Promise<InternalUser> {
-        const rawFetched = await keyv.get(this.id);
+        const rawFetched = await users.get(this.id);
         if (rawFetched === undefined) throw new Error("UNKNOWN_INTERNAL_USER");
         const fetched: InternalUser = Object.assign(new InternalUser(this.id), rawFetched);
         return fetched;
     }
 
     public async delete(): Promise<void> {
-        await keyv.delete(this.id);
+        await users.delete(this.id);
     }
 
     public async save(): Promise<true> {
-        return await keyv.set(this.id, this);
+        return await users.set(this.id, this);
     }
 
     public async updateRefreshToken(newRefreshToken: string): Promise<true> {
@@ -95,10 +90,8 @@ export class InternalUser {
         return true;
     }
 
-    private async _updateSession(newSession: Session): Promise<true> {
-        const fetchedThis: InternalUser = await this._fetch();
-        fetchedThis._session = newSession
-        return await fetchedThis.save();
+    private async _updateSession(newSession: SessionToken): Promise<true> {
+        return await sessions.set(this.id, newSession, InternalUser.TEN_DAYS_IN_MILLISECONDS);
     }
 
     private async _getNewAccessToken(): Promise<DiscordAccessTokenReponse> {
@@ -108,13 +101,8 @@ export class InternalUser {
         return freshTokenData;
     }
 
-    public async getNewSession(): Promise<Session> {
-        const newSession: Session = {
-            token: randomBytes(64).toString("base64url"),
-            expirationTimestamp: Date.now() + InternalUser.TEN_DAYS_IN_MILLISECONDS,
-            id: this.id
-        };
-
+    public async getNewSession(): Promise<SessionToken> {
+        const newSession = randomBytes(32).toString("base64url");
         await this._updateSession(newSession);
         return newSession;
     }
@@ -139,13 +127,11 @@ export class InternalUser {
 
     }
 
-    public async sessionTokenIs(token: string): Promise<boolean> {
-        const fetchedThis: InternalUser = await this._fetch();
-        return (fetchedThis._session.token === token);
+    public async sessionTokenIs(token: SessionToken): Promise<boolean> {
+        return ((await sessions.get(this.id)) === token);
     }
 
-    public async isSessionValid(): Promise<boolean> {
-        const fetchedThis: InternalUser = await this._fetch();
-        return (fetchedThis._session.expirationTimestamp > Date.now());
+    public async hasNonExpiredSession(): Promise<boolean> {
+        return ((await sessions.get(this.id)) !== undefined);
     }
 }
