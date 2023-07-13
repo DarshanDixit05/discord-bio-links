fea8163bd4ed606755e01e744e1a1a479efe5ca3
main <zzyg675@proton.me>
2023-07-13 15:17:13 +0400

    use cookies for sessions, error handling middleware support, switched multi language support to i18next

--- code/server/routes/settings.ts
+++ code/server/routes/settings.ts
@@ -1,21 +1,70 @@
 import { Router } from "express";
-import { join } from "path";
+import { join, resolve } from "path";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import { cfg } from "../../config.js";
+import { getSupportedMostPreferredLanguage } from "../../helpers/getSupportedMostPreferredLanguage.js";
+import { User } from "../../structures/User.js";
+import { InternalUser } from "../../structures/InternalUser.js";
+import cookieParser from "cookie-parser";
+
+const i18n = i18next.createInstance();
+
+await i18n.use(Backend).init({
+    fallbackLng: 'en',
+    ns: [
+        "footer",
+        "settings",
+        "header"
+    ],
+    backend: {
+        loadPath: resolve(cfg.directories.public, 'locales/{{lng}}/{{ns}}.json')
+    }
+});
+
+const translate = i18n.t;
 
 const settings = Router();
 
-settings.get("/", function (req, res) {
-    const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+settings.get("/", cookieParser(), async function (req, res, next) {
+    try {
+        const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+        const mostPreferredSupportedLanguage = getSupportedMostPreferredLanguage(acceptedLanguages);
 
-    for (const lang of acceptedLanguages) {
-        if (!res.headersSent) {
-            if (lang.includes("fr")) {
-                res.render(join("french", "settings.ejs"));
+        await i18n.changeLanguage(mostPreferredSupportedLanguage);
+
+        let user: User | null = null;
+        let authLink: string | null = null;
+
+        if (req.cookies.session && typeof req.cookies.session === "string") {
+            const userId = req.cookies.session.slice(0, req.cookies.session.indexOf(" "));
+
+            try {
+                user = await (new InternalUser(userId)).get();
+            } catch (err) {
+                console.error(err);
             }
         }
-    }
 
-    // Fallback language: English
-    if (!res.headersSent) res.render(join("english", "settings.ejs"));
+        res.render("settings.ejs", {
+            user,
+            lang: mostPreferredSupportedLanguage,
+            authLink,
+            origin: cfg.client.redirectUri,
+            translations: {
+                catchline: translate("catchline", { ns: "header" }),
+                logout: translate("logout", { ns: "settings" }),
+                delete: translate("delete", { ns: "settings" }),
+                main: translate("main", { ns: "footer" }),
+                sourceCode: translate("github", { ns: "footer" }),
+                donate: translate("donate", { ns: "footer" }),
+                guide: translate("guide", { ns: "footer" }),
+                settings: translate("settings", { ns: "footer" }),
+            }
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
 export default settings;
