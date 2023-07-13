471bcdbc9c80d10237166fb036f462dbb4c8064f
main <zzyg675@proton.me>
2023-07-13 15:17:15 +0400

    if envName is 'dev', the server will be launched at the specified local IP, useful for testing on mobiles/other devices on the network

--- code/index.ts
+++ code/index.ts
@@ -2,4 +2,10 @@ import { cfg } from "./config.js";
 import { appServer } from "./server/server.js";
 
 console.log(`Current 'cfg' object:`, cfg);
-appServer.listen(cfg.port, () => console.log(`Server launched and listening at localhost:${cfg.port}`));
+
+if (cfg.envName === "dev") {
+    const localIP = "192.168.1.15";
+    appServer.listen(cfg.port, localIP || "localhost", function () { console.log(`Server launched and listening at ${localIP}:${cfg.port} !`) });
+} else {
+    appServer.listen(cfg.port, () => console.log(`Server launched and listening at port ${cfg.port} !`));
+}
