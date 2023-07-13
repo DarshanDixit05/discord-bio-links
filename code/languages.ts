bd1204480771c86eeb788c9338e05ce9b919b273
main <zzyg675@proton.me>
2023-07-13 15:31:11 +0400

    add checks to know if a language actually has translations before saying it's supported

--- code/languages.ts
+++ code/languages.ts
@@ -1,4 +1,20 @@
-export const lang = {
+import { resolve } from "path";
+import { existsSync } from "fs";
+import { cfg } from "./config.js";
+
+const langs = {
     us: 1,
     fr: 1
-};
+}
+
+for (const lang of Object.keys(langs)) {
+    if (!existsSync(resolve(cfg.directories.public, "locales", lang))) {
+        throw new Error(`No translations in /public/locales/${lang} !`);
+    }
+}
+
+function getSupportedMostPreferredLanguage(preferredLangs: string[]): string {
+    return preferredLangs.find((lang: string) => lang in langs) || "us";
+}
+
+export { langs, getSupportedMostPreferredLanguage };
