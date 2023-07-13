5aa61357fb3297a9fcae409eb4419864ce673e5d
main <zzyg675@proton.me>
2023-07-13 15:17:21 +0400

    add JSDoc

--- public/js/helpers/getCountryFlagFor.js
+++ public/js/helpers/getCountryFlagFor.js
@@ -1,3 +1,8 @@
+/**
+ * Gets the emoji flag from a country code.
+ * @param {string} code 
+ * @returns {string}
+ */
 export function getCountryFlagFor(code) {
     return String.fromCodePoint(...Array.from(code.toUpperCase()).map(char => char.charCodeAt() + 127397));
 }
