09c9fd1c3500e9938418d870d85004d16ae3523b
main <zzyg675@proton.me>
2023-07-13 15:17:13 +0400

    error handling middleware support, switched multi language support to i18next

--- code/server/routes/guide.ts
+++ code/server/routes/guide.ts
@@ -1,21 +1,49 @@
 import { Router } from "express";
-import { join } from "path";
+import { join, resolve } from "path";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import { cfg } from "../../config.js";
+import { getSupportedMostPreferredLanguage } from "../../helpers/getSupportedMostPreferredLanguage.js";
+
+const i18n = i18next.createInstance();
+
+await i18n.use(Backend).init({
+    fallbackLng: 'en',
+    ns: [
+        "footer",
+        "header"
+    ],
+    backend: {
+        loadPath: resolve(cfg.directories.public, 'locales/{{lng}}/{{ns}}.json')
+    }
+});
+
+const translate = i18n.t;
 
 const guide = Router();
 
-guide.get("/", function (req, res) {
-    const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+guide.get("/", async function (req, res, next) {
+    try {
+        const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+        const mostPreferredSupportedLanguage = getSupportedMostPreferredLanguage(acceptedLanguages);
 
-    for (const lang of acceptedLanguages) {
-        if (!res.headersSent) {
-            if (lang.includes("fr")) {
-                res.render(join("french", "guide.ejs"));
+        await i18n.changeLanguage(mostPreferredSupportedLanguage);
+
+        res.render("guide.ejs", {
+            lang: mostPreferredSupportedLanguage,
+            origin: cfg.client.redirectUri,
+            translations: {
+                catchline: translate("catchline", { ns: "header" }),
+                main: translate("main", { ns: "footer" }),
+                sourceCode: translate("github", { ns: "footer" }),
+                donate: translate("donate", { ns: "footer" }),
+                guide: translate("guide", { ns: "footer" }),
+                settings: translate("settings", { ns: "footer" }),
             }
-        }
+        });
+    } catch (err) {
+        next(err);
     }
-
-    // Fallback language: English
-    if (!res.headersSent) res.render(join("english", "guide.ejs"));
 });
 
 export default guide;
--- code/server/routes/view.ts
+++ code/server/routes/view.ts
@@ -1,27 +1,64 @@
 import { Router } from "express";
 import { cfg } from "../../config.js";
-import { isValidDiscordId } from "../../helpers/isValidDiscordId.js";
-import { join } from "path";
+import { resolve } from "path";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import { getSupportedMostPreferredLanguage } from "../../helpers/getSupportedMostPreferredLanguage.js";
+import { User } from "../../structures/User.js";
+import { InternalUser } from "../../structures/InternalUser.js";
+
+const i18n = i18next.createInstance();
+
+await i18n.use(Backend).init({
+    fallbackLng: 'en',
+    ns: [
+        "footer",
+        "view",
+        "errors",
+        "header"
+    ],
+    backend: {
+        loadPath: resolve(cfg.directories.public, 'locales/{{lng}}/{{ns}}.json')
+    }
+});
+
+const translate = i18n.t;
 
 const view = Router();
 
-view.get("/view/:id", function (req, res) {
-    const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+view.get("/:id", async function (req, res, next) {
+    try {
+        const acceptedLanguages = req.acceptsLanguages().map(lang => lang.toLowerCase());
+        const mostPreferredSupportedLanguage = getSupportedMostPreferredLanguage(acceptedLanguages);
 
-    for (const lang of acceptedLanguages) {
-        if (!res.headersSent) {
-            if (lang.includes("fr")) {
-                res.render(
-                    join("french", "view.ejs")
-                );
-            }
+        await i18n.changeLanguage(mostPreferredSupportedLanguage);
+
+        let user: User | null = null;
+
+        try {
+            user = await (new InternalUser(req.params.id)).get();
+        } catch (err) {
+            console.error(err);
         }
-    }
 
-    // Fallback language: English
-    if (!res.headersSent) res.render(
-        join("english", "view.ejs")
-    );
+        res.render("view.ejs", {
+            user,
+            lang: mostPreferredSupportedLanguage,
+            translations: {
+                catchline: translate("catchline", { ns: "header" }),
+                userNotFound: translate("user not found", { ns: "errors" }),
+                main: translate("main", { ns: "footer" }),
+                sourceCode: translate("github", { ns: "footer" }),
+                donate: translate("donate", { ns: "footer" }),
+                guide: translate("guide", { ns: "footer" }),
+                settings: translate("settings", { ns: "footer" }),
+                usernameDisplay: translate("username display", { ns: "view", username: (user === null) ? "" : user.username }),
+                turnRawViewOn: translate("turn raw view on", { ns: "view" })
+            }
+        });
+    } catch (err) {
+        next(err);
+    }
 });
 
 export default view;
