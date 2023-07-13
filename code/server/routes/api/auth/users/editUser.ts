08a1145ae3fbfcd7ea69afdae3d20394fde8fb96
main <zzyg675@proton.me>
2023-07-13 15:31:12 +0400

    add check to know if a bio is in a supported language

--- code/server/routes/api/auth/users/editUser.ts
+++ code/server/routes/api/auth/users/editUser.ts
@@ -3,6 +3,7 @@ import { InternalUser } from "../../../../../structures/InternalUser.js";
 import DOMPurify from "isomorphic-dompurify";
 import { isBiography } from "../../../../../structures/Biography.js";
 import { User } from "../../../../../structures/User.js";
+import { langs } from "../../../../../languages.js";
 
 const editUser = Router();
 
@@ -14,6 +15,10 @@ editUser.post("/", async function (req, res, next) {
         for (const bio of Object.values(req.body)) {
             if (!isBiography(bio)) throw new Error("INVALID_REQUEST");
             bio.text = DOMPurify.sanitize(bio.text.trim());
+        }
+
+        for (const langKey of Object.keys(req.body)) {
+            if (langKey in langs) throw new Error("UNSUPPORTED_LANGUAGE");
         }
 
         await internalUser.editBios(req.body);
