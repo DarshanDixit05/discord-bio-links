ed5633ec9f13bb209cc43a9c27ad20b5d7373e8b
xefyr675dev <xefyr675+dev@gmail.com>
2023-07-14 00:33:50 +0400

    switched from JSON5 to HJSON, because JSON5 didn't allow line breaks without a '\'

--- public/locales/finalize.js
+++ public/locales/finalize.js
@@ -1,20 +1,23 @@
 // @ts-check
 // This is a script to convert all the JSON5 files to standard JSON.
-import JSON5 from 'json5';
 import { readdirSync, readFileSync, writeFileSync } from "fs";
-import { resolve } from "path";
+import { dirname, resolve } from "path";
+import * as HJSON from "hjson";
+import { fileURLToPath } from "url";
 
-const inDir = resolve("json5");
-const outDir = resolve("json");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const inDir = resolve(__dirname, "hjson");
+const outDir = resolve(__dirname, "json");
 
 console.log(`Starting.`);
 
 for (const lang of readdirSync(inDir)) {
     for (const file of readdirSync(resolve(inDir, lang))) {
         writeFileSync(
-            resolve(outDir, lang, file.replace(".json5", ".json")),
+            resolve(outDir, lang, file.replace(".hjson", ".json")),
             JSON.stringify(
-                JSON5.parse(
+                HJSON.parse(
                     readFileSync(resolve(inDir, lang, file), "utf8")
                 )
             )
