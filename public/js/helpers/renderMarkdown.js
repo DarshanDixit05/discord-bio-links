49982fcf4b1089b886c73bcfe789de8a6075dbc5
xefyr675 <xefyr675.dev@proton.me>
2023-07-16 22:50:35 +0400

    workarounds for proper horizontal rule display

--- public/js/helpers/renderMarkdown.js
+++ public/js/helpers/renderMarkdown.js
@@ -10,6 +10,24 @@ const headerLevelStarts = {
 };
 
 /**
+ * Workarounds to ensure proper display.
+ * @param {string} text 
+ * @returns {string}
+ */
+function prepareMarkdown(text) {
+    return text
+        // For markdown to make a line break a line break, it needs 2 spaces before the line break.
+        .replace(/\n/g, "  \n")
+        // Without specifying multiples line breaks as <br> tags, they would get removed.
+        .replace(/  \n  \n/g, "<br><br>  \n  \n")
+        // The following are horizontal rules workarounds; because of the previous line break workarounds,
+        // markdown horizontal rules ('***', '___' or '---') had <br> tags on the same line so they weren't parsed as horizontal rules.
+        .replace(/\n\*\*\*<br><br>/g, "\n***\n")
+        .replace(/\n---<br><br>/g, "\n---\n")
+        .replace(/\n___<br><br>/g, "\n___\n");
+}
+
+/**
  * Renders the given markdown text with the specified headerLevelStart (or 1 by default) within the specified element.
  * @param {HTMLElement} element 
  * @param {string} markdownText 
@@ -19,7 +37,6 @@ export function renderMarkdown(element, markdownText, headerLevelStart = 1) {
     const options = {
         tables: true,
         tasklists: true,
-        simpleLineBreaks: true,
         openLinksInNewWindow: true,
         emoji: true,
         strikethrough: true,
@@ -28,13 +45,11 @@ export function renderMarkdown(element, markdownText, headerLevelStart = 1) {
 
     // @ts-ignore - Showdown should be available because its script *should* included in the HTML file.
     const converter = new showdown.Converter(options);
-    // @ts-ignore
-    const parsed = markdownText
-        .replace(/\n/g, "  \n")
-        .replace(/  \n  \n/g, "<br><br>  \n  \n");
-
 
     // @ts-ignore - DOMPurify should be available because its script *should* included in the HTML file.
-    element.innerHTML = DOMPurify.sanitize(converter.makeHtml(parsed));
-    console.log(element.innerHTML);
+    element.innerHTML = DOMPurify.sanitize(
+        converter.makeHtml(
+            prepareMarkdown(markdownText)
+        )
+    );
 }
