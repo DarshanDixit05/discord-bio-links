fd7ca97633daa218692f8a3405641f38fce9e7f9
xefyr675 <xefyr675.dev@proton.me>
2023-07-16 18:19:10 +0400

    moved translation finalization script

--- code/helpers/finalizeTranslations.ts
+++ code/helpers/finalizeTranslations.ts
@@ -1,16 +1,16 @@
-// @ts-check
 // This is a script to convert all the JSON5 files to standard JSON.
 import { readdirSync, readFileSync, writeFileSync } from "fs";
 import { dirname, resolve } from "path";
 import * as HJSON from "hjson";
 import { fileURLToPath } from "url";
+import { cfg } from "../config.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const inDir = resolve(__dirname, "hjson");
-const outDir = resolve(__dirname, "json");
+const inDir = resolve(cfg.directories.public, "locales", "hjson");
+const outDir = resolve(cfg.directories.translations);
 
-console.log(`Starting.`);
+console.log(`Converting HJSON translations to JSON.`);
 
 for (const lang of readdirSync(inDir)) {
     for (const file of readdirSync(resolve(inDir, lang))) {
