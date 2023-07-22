5f291a819cbc671d7f74c45bd672e112a8c9efa3
xefyr675 <xefyr675@gmail.com>
2023-07-22 19:32:20 +0400

    useless

--- public/js/pages/guide.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-import { renderMarkdown } from "../helpers/renderMarkdown.js";
-
-window.addEventListener("DOMContentLoaded", function () {
-    const main = document.getElementById("main");
-    if (main) renderMarkdown(main, main.innerHTML);
-});
